b004e046442f2f12ec301baa3fe00d87efd3d172
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-08-02 15:53:33 +0530

    added redis backend cache

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ShortURL = require('./models/shortURL')
 const multer = require('multer')
 const cors = require('cors');
 const shortid = require('shortid')
+const Redis = require("redis");
 
 const app = express()
 app.use(express.urlencoded({extended : true}))    
@@ -15,7 +16,17 @@ mongoose.connect(process.env.DATABASE_URL)
 app.use(cors());
 app.use(express.json())
 
+const redisClient = Redis.createClient({
+    host: process.env.REDIS_HOSTNAME,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+})
+redisClient.on('connect' , () => console.log("connected to redis !"))
+redisClient.on('error' , (err) => console.log(err))
+const DEFAULT_EXPIRY = 3600;
+
 app.post('/upload' ,  upload.single("file") ,async (req, res) => {
+    
     const fileData = {
         path  : req.file.path ,
         originalName: req.file.originalname
@@ -34,8 +45,13 @@ app.post('/upload' ,  upload.single("file") ,async (req, res) => {
 app.route('/download/:id').get(handleDownload).post(handleDownload)
 
 async function handleDownload(req, res){
-        const file = await File.findById(req.params.id)
-        console.log(req.body.password)
+
+        const file = await getOrSetCache(`file?id=${req.params.id}` , async()=>{
+            const data =  await File.findById(req.params.id)
+            return data
+        })
+
+        
         if(file.password != null)
         {
             if(req.body.password == null || req.body.password == "")
@@ -52,17 +68,18 @@ async function handleDownload(req, res){
             }
         }
     
-        file.downloadCount++;
-        await file.save()
-        // const data = {passwordNeeded : file.password!=null , path:file.path , originalName : file.originalName}
-        
+    
         // res.json({path : file.path , originalName:  file.originalName})
         res.download(file.path , file.originalName)
     
 }
 
 app.get('/file/:id' , async (req,res) => {
-    const file = await File.findById(req.params.id)
+    // const file = await File.findById(req.params.id)
+    const file = await getOrSetCache(`file?id=${req.params.id}` , async()=>{
+        const data =  await File.findById(req.params.id)
+        return data
+    })
    
     const data = {passwordNeeded : file.password!=null , originalName : file.originalName}
     
@@ -80,18 +97,37 @@ app.post('/short-url' , async (req, res)=>{
 
 app.get('/su/:shorturl' , async (req,res)=>{
     const shorturl = req.params.shorturl
-    const urldata = await ShortURL.findOne({ short : shorturl })
+    // const urldata = await ShortURL.findOne({ short : shorturl })
+    const urldata = await getOrSetCache(`urldata?id=${req.params.shorturl}` , async()=>{
+        const data =  await ShortURL.findOne({ short : shorturl })
+        return data
+    })
 
     if(urldata == null)
     {
         return res.status(404).send("url does not exist !")
     }
 
-    urldata.clicks++;
-    urldata.save()
+    // urldata.clicks++;
+    // urldata.save()
 
     res.redirect(urldata.full)
 })
 
+function getOrSetCache(key , cb){
+    return new Promise((resolve , reject) => {
+        redisClient.get(key , async (error,data) => {
+            if(error) return reject(error)
+            if(data != null)
+            {
+                resolve(JSON.parse(data));
+            }
+
+            const newData = await cb()
+            redisClient.setex(key , DEFAULT_EXPIRY , JSON.stringify(newData))
+            resolve(newData)
+        })
+    })
+}
 
 app.listen(process.env.PORT , () => console.log("Server is up ..."))
